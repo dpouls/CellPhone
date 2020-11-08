8ed3a8a8e6c21336d29d851e68149797d10adeaf
Dallin <dallinpoulson@gmail.com>
2020-11-07 19:53:13 -0700

    starting project and pushing to github

---
+++ Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CellPhone.ViewModels;
+
+namespace CellPhone.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
